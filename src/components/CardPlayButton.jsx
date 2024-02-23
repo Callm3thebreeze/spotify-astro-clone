e2f7aea222f438e44fcd26cada9ee64d426b790e
Jesús Villar <jesusvillarjimenez@gmail.com>
2024-02-23 12:03:11 +0100

    independent card buttons simple

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -2,19 +2,22 @@ import { Pause, Play } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
 export function CardPlayButton({ id }) {
-  const { currentSong, isPlaying, setIsPlaying, setCurrentMusic } =
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
   const handleClick = () => {
+    setCurrentMusic({ playlist: { id } });
     setIsPlaying(!isPlaying);
   };
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   );
 }
